21569e9a6e9056581824c691f0eeaacf12d77e60
Goran <goranfrance@hotmail.com>
2021-03-22 21:46:26 +0000

    afsd

--- my-app/src/context/GlobalState.js
+++ my-app/src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react'
+import AppReducer from './AppReducer'
 
 //initial state
 
@@ -9,4 +10,20 @@ const initialState = {
         { id: 3, text: 'Book', amount: -10 },
         { id: 4, text: 'Camera', amount: 150 }
     ]
+}
+
+// Create context
+
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+
+export const GlobalProvider = ({ children }) => {
+ const [state, dispatch] = useReducer(AppReducer, initialState);
+
+ return (<GlobalContext.Provider value={{
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>);
 }
